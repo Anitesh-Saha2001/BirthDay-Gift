0d4d790688c0c5009e390424c9b766f89bb40e40
Anitesh Saha <aniteshsaha@gmail.com>
2026-05-27 00:49:25 +0530

    Update unlockDate to a specific future date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const targetDate = new Date("2026-05-28T00:00:00+05:30").getTime();
-const unlockDate = Date.now() + (2 * 60 * 1000);
+const unlockDate = new Date("2026-05-28T00:30:00+05:30").getTime();
 
 const units = {
   days: document.querySelector("#days"),
